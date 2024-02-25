deacd30c4e20e0617b25ab30ee3748e1f6a39c3a
John Staveley <john@staveley.org>
2024-02-25 06:28:57 +0000

    Got grpc web call working

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -3,12 +3,17 @@ using GrpcService.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors();
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseGrpcWeb();
-app.MapGrpcService<TelemetryService>().EnableGrpcWeb().RequireCors("AllowAll");
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGrpcService<TelemetryService>().EnableGrpcWeb().RequireCors(builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+});
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909").RequireCors("AllowAll");
+app.UseCors();
 app.Run();
