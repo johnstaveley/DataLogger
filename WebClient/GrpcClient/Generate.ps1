6d0630fd1b00f6cd21658cf6c670e90384a5fcaf
John Staveley <john@staveley.org>
2024-02-25 05:21:48 +0000

    Create files in root directory of client

--- WebClient/GrpcClient/Generate.ps1
+++ WebClient/GrpcClient/Generate.ps1
@@ -1,4 +1,9 @@
+Remove-Item datalog_pb.js -force
+Remove-Item datalog_grpc_web_pb.js -force
 protoc.exe -I ..\..\ `
 	--js_out=import_style=commonjs:.\ `
 	--grpc-web_out=import_style=commonjs,mode=grpcwebtext:.\ `
 	..\..\GrpcService\Protos\datalog.proto
+$files = Get-ChildItem 'GrpcService/Protos/*'
+Get-ChildItem $files | Move-Item -Destination { $_.Directory.Parent.Parent.FullName }
+rmdir GrpcService -recurse -force
